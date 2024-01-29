6c8eea33c353583f82079c766acf64a0dde68480
Subash0711 <109809088+Subash0711@users.noreply.github.com>
2024-01-29 12:10:23 +0530

    update space buckets

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -44,7 +44,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     
     'user',
-    'blog_app'
+    'blog_app',
+    'storages',
 ]
 
 MIDDLEWARE = [
@@ -62,7 +63,7 @@ ROOT_URLCONF = 'blog.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'static')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -145,6 +146,21 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+AWS_ACCESS_KEY_ID = 'DO00KLZBJ9NQR4CF7Q44'
+AWS_SECRET_ACCESS_KEY = 'RedkozWeSdHgzqkc07TU0RK2TXodmhc6TwNjLhiHQrk'
+AWS_STORAGE_BUCKET_NAME = 'django.blognest'
+AWS_S3_ENDPOINT_URL = 'https://django.blognest.nyc3.digitaloceanspaces.com'
+AWS_S3_OBJECT_PARAMETERS = {
+    'CacheControl': 'max-age=86400',
+}
+AWS_LOCATION = 'blognest'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
+STATIC_URL = 'https://%s/%s/' % (AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
